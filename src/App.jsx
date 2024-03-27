44b6eff8de43d3cc8d671d11264afa856d91e50c
Hamdi-Hossam <hamdihossam461@gmail.com>
2024-03-27 22:17:11 +0200

    terms and conditions fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Landing from './pages/DesktopOne/index';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import TermsAndConditions from "pages/termsAndConditions/termsAndConditions";
+import TermsAndConditions from "./pages/termsAndConditions/termsAndConditions";
 
 function App() {
   return (
